362e0a1c712f3bbbcb7599c3978ca57cc0b3f87b
Vaibhav Prakash <58112154+vbhvprksh@users.noreply.github.com>
2021-08-28 10:58:42 +0530

    base folder structure done

---
+++ home/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+import './scss/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
